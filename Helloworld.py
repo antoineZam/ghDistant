39c78936720ef1ee1b167bc4a0837d1ba73d9dd3
Antoine <azamord1@gmail.com>
2025-05-12 16:21:25 +0200

    C3 : mise à jour taille fenêtre et ajustement du texte

--- Helloworld.py
+++ Helloworld.py
@@ -5,22 +5,19 @@ from datetime import datetime
 def main():
     window = tk.Tk()
     window.title(HELLO_WORLD_TITLE)
-    window.geometry("200x200")
+    window.geometry("350x200")
     window.configure(bg="white")  # Fond par défaut (modifié dans les commits)
 
     label = tk.Label(
         window,
         text=HELLO_WORLD_TEXT,
-        fg="black",     # Couleur du texte (bleu dans un commit futur)
-        bg="yellow",    # Fond du label (modifié dans les commits)
+        fg="black",
+        bg="yellow",
         font=("Arial", 12),
         justify="center"
     )
     label.pack(expand=True)
-
-    # Exemple d'ajout ultérieur (ex: ajout de la date dans un commit futur)
-    # date_label = tk.Label(window, text=datetime.now().strftime("%Y-%m-%d"))
-    # date_label.pack()
+    
 
     window.mainloop()
 
